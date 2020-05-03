2d9610b704a9b6785a730812f570af7ac5ca0785
Carsten Kraus <ck@omikron.net>
2020-05-03 13:55:33 +0200

    Umstrukturierung auf funktionen deths und recovered werden eingelesen
    Vorhersage auf active statt cases erweitert
    (auch nicht korrekt, weil zwar actives
    anstecken
    recovern
    sterben
    - aber mit unterschiedlichem Zeithorizont
    --> die Vorhersage stimmt so also auch nicht wirklich

--- Covid19.R
+++ Covid19.R
@@ -1,20 +1,51 @@
 library(tidyverse)
 library(readr)
 
+f = function(dset)
+{
+  dset$wachstumAlt=(dset[,l-5]/dset[,l-10])^0.2
+  dset$wachstum=(dset[,l]/dset[,l-5])^0.2
+  dset$change=(dset$wachstum-dset$wachstumAlt)
+  dset$einMonat=dset[,l]*dset$wachstum^30
+  dset$zweiMonate=dset$einMonat*(dset$wachstum/2+0.5)^30
+  return(dset)
+}
+
+display = function(dset)
+{
+  dset$wachstum=round(dset$wachstum*100-100,2)
+  dset$wachstumAlt=round(dset$wachstumAlt*100-100,2)
+  dset$change=(dset$wachstum-dset$wachstumAlt)
+  dset$einMonat=round(dset$einMonat,0)
+  dset$zweiMonate=round(dset$zweiMonate,0)
+  
+  View(dset[dset[,l]>1000&is.na(dset$`Province/State`),c(1:2,l:(length(dset)))])
+}
+
+time_series_covid19_recovered_global <- read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+#View(time_series_covid19_recovered_global)
+recovered = f(time_series_covid19_recovered_global)
+
 time_series_covid19_confirmed_global <- read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 #View(time_series_covid19_confirmed_global)
-
 covid = time_series_covid19_confirmed_global
+cases = f(covid)
+cases = merge(recovered[1:2], cases, by=c("Province/State","Country/Region"))
 
-l=length(covid)
-covid$wachstumAlt=(covid[,l-5]/covid[,l-10])^0.2
-covid$wachstum=(covid[,l]/covid[,l-5])^0.2
-covid$change=(covid$wachstum-covid$wachstumAlt)
-covid$einMonat=covid[,l]*covid$wachstum^30
-covid$zweiMonate=covid$einMonat*(covid$wachstum/2+0.5)^30
+time_series_covid19_deaths_global <- read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+#View(time_series_covid19_deaths_global)
+deaths = f(time_series_covid19_deaths_global)
+deaths = merge(recovered[1:2], deaths, by=c("Province/State","Country/Region"))
 
+recovered = merge(deaths[1:2], recovered, by=c("Province/State","Country/Region"))
 
-View(covid[covid[,l]>1000&is.na(covid$`Province/State`),c(1:2,l:(length(covid)))])
+display(cases)
+
+active[,1:4] = cases[,1:4]
+active[,5:l] = cases[,5:l] - deaths[,5:l] - recovered[,5:l]
+active = f(active)
+
+display(active)
 
 
 #Einbauen: Anzahl Corona-Tests berücksichtigen
